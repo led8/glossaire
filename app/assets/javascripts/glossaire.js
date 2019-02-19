b8e893644418e22ee23bf44e69f29080010e7bb2
Arthur Dhuy <a.dhuy@orange.fr>
2019-02-19 18:48:01 +0100

    Fix jquery Event(click) on div#collapse

--- app/assets/javascripts/glossaire.js
+++ app/assets/javascripts/glossaire.js
@@ -1,5 +1,18 @@
 function documentReady(jQuery){
-  console.log($('h1').text());
+  $('.letters-item a').each(function(index){
+    $(this).on('click', function(event){
+      event.preventDefault();
+      var clickedElement = $(this).text();
+      if(clickedElement){
+        console.log($("h4.panel-title").text === clickedElement);
+        // $("h4.panel-title").each(function(){
+          //console.log($("h4.panel-title").text());
+        // });
+        $(`div#collapse${index+1}`).toggleClass('in');
+      };
+    });
+  });
 }
-
 $(document).ready(documentReady);
+
+
