1e939ed286df90abc4181b393a4cab390c74d2fc
Arthur Dhuy <a.dhuy@orange.fr>
2019-02-21 11:22:12 +0100

    Refacto glossaire view and corrected text

--- app/assets/javascripts/glossaire.js
+++ app/assets/javascripts/glossaire.js
@@ -1,14 +1,14 @@
 function documentReady(jQuery){
-  function eachLetter(index){
-    function click(event){
-      var clickedElement = $(this).text();
-      if(clickedElement){
+  function eachLetterIndex(index){
+    function clickElementIndex(){
+      var clickedElementIndex = $(this).text();
+      if(clickedElementIndex){
         $('.panel-collapse.collapse.in.glossary-content').removeClass('in');
         $(`div#collapse${index+1}`).toggleClass('in');
       };
-    }
-    $(this).on('click', click);
+    };
+    $(this).on('click', clickElementIndex);
   };
-  $('.letters-item a').each(eachLetter);
+  $('.letters-item a').each(eachLetterIndex);
 }
 $(document).ready(documentReady);
